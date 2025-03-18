b460173ec9be3146df5866f4a21d89ebd9cf74d5
foclabroc <67229198+foclabroc@users.noreply.github.com>
2025-03-18 16:00:29 +0100

    Update foclabroc-toolbox.sh

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -4,3 +4,4 @@ rm /tmp/script.sh 2>/dev/null
 curl -L https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/app/launch-toolbox.sh -o /tmp/scripttb.sh
 dos2unix /tmp/scripttb.sh 2>/dev/null 
 bash /tmp/scripttb.sh
+exit 0
