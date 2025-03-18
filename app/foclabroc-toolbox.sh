9faa4fe8f5894940c1dcd2ca26e7b935959f51a9
foclabroc <67229198+foclabroc@users.noreply.github.com>
2025-03-18 13:43:38 +0100

    Update foclabroc-toolbox.sh

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -4,5 +4,4 @@ echo "Add script to temp then execute"
 rm /tmp/script.sh 2>/dev/null
 curl -L foclabroc.short.gy/toolbox -o /tmp/scripttb.sh
 dos2unix /tmp/scripttb.sh 2>/dev/null 
-chmod a+x /tmp/scripttb.sh 2>/dev/null
 bash /tmp/scripttb.sh
