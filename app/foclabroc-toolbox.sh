89e1e6e5db22479c4c57613e45a86f6d58d16a52
foclabroc <foclabroc@hotmail.fr>
2025-03-18 12:36:42 +0100

    ....

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 echo "Add script to temp then execute"
 rm /tmp/script.sh 2>/dev/null
-curl -L foclabroc.short.gy/toolbox -o /tmp/script.sh
-dos2unix /tmp/script.sh 2>/dev/null 
-chmod a+x /tmp/script.sh 2>/dev/null
-bash /tmp/script.sh
+curl -L foclabroc.short.gy/toolbox -o /tmp/scripttb.sh
+dos2unix /tmp/scripttb.sh 2>/dev/null 
+chmod a+x /tmp/scripttb.sh 2>/dev/null
+bash /tmp/scripttb.sh
