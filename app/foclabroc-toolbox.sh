684f1ac50fbc98a378e5e40f26fd74d2be0ef1d0
foclabroc <67229198+foclabroc@users.noreply.github.com>
2025-03-18 16:41:14 +0100

    Update foclabroc-toolbox.sh

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-if [ ! -t 1 ]; then
-    echo "No interactive terminal detected. Opening new terminal..."
-    xterm -- bash -c "bash /tmp/scripttb.sh; exec bash"
-    exit 0
-fi
-
 echo "Add script to temp then execute"
-rm /tmp/script.sh 2>/dev/null
+rm /tmp/scripttb.sh 2>/dev/null
 curl -L https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/app/launch-toolbox.sh -o /tmp/scripttb.sh
 dos2unix /tmp/scripttb.sh 2>/dev/null
 
-# Exécution du script dans le terminal actuel si interactif
-bash /tmp/scripttb.sh
+# Ouvrir un terminal interactif et exécuter le script
+xterm -hold -e "bash /tmp/scripttb.sh"
