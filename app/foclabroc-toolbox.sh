73c3e7ee790a4b5b7d5f1715a2e529e368a78232
foclabroc <foclabroc@hotmail.fr>
2025-03-18 12:29:53 +0100

    ....

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Add script to temp then execute"
-rm /tmp/toolbox 2>/dev/null
-wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/toolbox https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/app/launch-toolbox.sh
-dos2unix /tmp/toolbox 2>/dev/null 
-chmod 777 /tmp/toolbox 2>/dev/null
-bash /tmp/toolbox
+rm /tmp/script.sh 2>/dev/null
+curl -L foclabroc.short.gy/toolbox -o /tmp/script.sh
+dos2unix /tmp/script.sh 2>/dev/null 
+chmod a+x /tmp/script.sh 2>/dev/null
+bash /tmp/script.sh
