2b357e31f58ca247bf938cb4e066241a58111cdf
foclabroc <67229198+foclabroc@users.noreply.github.com>
2025-03-18 16:15:03 +0100

    Update foclabroc-toolbox.sh

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+set -o pipefail
 echo "Add script to temp then execute"
 rm /tmp/scripttb.sh 2>/dev/null
 curl -L https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/app/launch-toolbox.sh -o /tmp/scripttb.sh
