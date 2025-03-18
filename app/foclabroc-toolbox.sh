eb6de12f49323334c7fd19b64398f686e4c7cb1a
foclabroc <foclabroc@hotmail.fr>
2025-03-18 11:53:41 +0100

    test for curl

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+echo "Add script to temp then execute"
+rm /tmp/toolbox 2>/dev/null
+wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/toolbox https://github.com/foclabroc/toolbox/blob/main/app/foclabroc-toolbox.sh
+dos2unix /tmp/toolbox 2>/dev/null 
+chmod 777 /tmp/toolbox 2>/dev/null
+bash /tmp/toolbox
+        ;;
 # Fichier de log des erreurs
 LOG_FILE="/tmp/install_errors.log"
 
