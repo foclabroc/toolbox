7c11655b4f027b2c3a9390c34ef3df117fd77522
foclabroc <foclabroc@hotmail.fr>
2025-03-20 10:04:43 +0100

    install-to-port

---
+++ app/install-to-port.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Get the machine hardware name
+architecture=$(uname -m)
+
+# Check if the architecture is x86_64 (AMD/Intel)
+if [ "$architecture" != "x86_64" ]; then
+    echo "This script only runs on AMD or Intel (x86_64) CPUs, not on $architecture."
+    exit 1
+fi
+
+# Check if /userdata/system/pro does not exist and create it if necessary
+if [ ! -d "/userdata/system/pro" ]; then
+    mkdir -p /userdata/system/pro
+fi
+
+
+echo "installing foclabroc-toolbox to port folder..."
+sleep 3
+# Add Foclabroc-tool.sh to "ports"
+curl -L https://github.com/foclabroc/toolbox/blob/main/app/foclabroc-tools.sh -o /userdata/roms/ports/foclabroc-tools.sh
+
+# Add Foclabroc-tool.keys to "ports"
+wget  https://github.com/foclabroc/toolbox/blob/main/app/foclabroc-tools.keys -P /userdata/roms/ports/
+
+# Set execute permissions for the downloaded scripts
+chmod +x /userdata/roms/ports/foclabroc-tools.sh
+
+
+killall -9 emulationstation
+
+sleep 1
+
+
+echo "Finished."
