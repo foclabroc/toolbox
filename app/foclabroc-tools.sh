74c89cb250988ff98b10f3a7b209942ef9667541
foclabroc <foclabroc@hotmail.fr>
2025-03-20 09:59:27 +0100

    test..

--- app/foclabroc-tools.sh
+++ app/foclabroc-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Add script to temp then execute"
 rm /tmp/scripttool.sh 2>/dev/null
-curl -L https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/app/launch-toolbox.sh -o /tmp/scripttool.sh
+curl -L https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/app/foclabroc-toolbox-ports.sh -o /tmp/scripttool.sh
 dos2unix /tmp/scripttool.sh 2>/dev/null
 
 # Ouvrir un terminal interactif et exécuter le script
