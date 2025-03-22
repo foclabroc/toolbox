c54fb54856547cc3c1a88e02d3e48a7945509417
foclabroc <foclabroc@hotmail.fr>
2025-03-22 11:09:14 +0100

    test in

--- app/foclabroc-toolbox.sh
+++ app/foclabroc-toolbox.sh
@@ -36,6 +36,75 @@ echo -e "\e[0m"
 sleep 3
 }
 
+tools_options() {
+  show_message() {
+    dialog --msgbox "$1" 6 50
+  }
+
+  # Fonction pour exécuter l'enregistrement
+  start_recording() {
+    # Lancer batocera-record en arrière-plan
+    batocera-record &>/dev/null &
+    RECORD_PID=$!
+
+    # Afficher la fenêtre avec un bouton Stop
+    CHOICE=$(dialog --title "Capture vidéo" --backtitle "Foclabroc Toolbox" \
+      --no-items --stdout \
+      --menu "Capture vidéo en cours appuyez sur stop pour terminer..." 15 60 1 \
+      "Stop Capture")
+
+    case $CHOICE in
+      1)
+        # Envoyer le signal SIGINT pour arrêter l'enregistrement
+        killall ffmpeg
+        ;;
+    esac
+
+    # Afficher le message de fin
+    show_message "Capture vidéo enregistrée dans le dossier Recordings avec succès."
+  }
+
+  # Fonction pour afficher le menu principal
+  main_menu() {
+    while true; do
+      CHOICE=$(dialog --menu "Choisissez une option" 15 80 4 \
+        1 "[Screenshot] -> Prendre des captures d'écran de Batocera." \
+        2 "[Reload]     -> Actualiser la liste des jeux." \
+        3 "[Record]     -> Capturer des vidéos de l'écran de Batocera" \
+        4 "[Retour]     -> Retour au menu principal de la toolbox" \
+        2>&1 >/dev/tty)
+
+      case $CHOICE in
+        1)
+          # Option Screenshot
+          batocera-screenshot
+          show_message "Screenshot enregistré dans le dossier Screenshots avec succès."
+          ;;
+        2)
+          # Option Reload
+          curl http://127.0.0.1:1234/reloadgames
+          show_message "Liste des jeux actualisée avec succès."
+          ;;
+        3)
+          # Option Record
+          start_recording
+          ;;
+        4)
+          # Retour
+          dialog --title "Retour" --msgbox "Retour au menu principal." 6 40
+          break
+          ;;
+        *)
+          # Quitter
+          break
+          ;;
+      esac
+    done
+  }
+
+  main_menu
+}
+
 # Vérification de la connexion Internet
 check_internet() {
     if ! ping -c 1 8.8.8.8 &>/dev/null; then
@@ -98,7 +167,7 @@ main_menu() {
                 ;;
             4)
                 clear
-                wget -q --tries=30 --no-check-certificate -O /tmp/runner https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/bat-tools/bat-tools.sh && chmod +x /tmp/runner && bash /tmp/runner
+                tools_options
                 ;;
             5)
                 confirm_install "Wine Custom" || continue
