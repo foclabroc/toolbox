9489570004bb3f5e186b54a2085f54fc57560c63
foclabroc <foclabroc@hotmail.fr>
2025-03-25 11:30:54 +0100

    ...

--- kodi/pack_kodi.sh
+++ kodi/pack_kodi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+clear
 # Définir l'URL du fichier ZIP
 ZIP_URL="https://github.com/foclabroc/toolbox/releases/download/Fichiers/kodi.zip"
 
@@ -13,7 +14,7 @@ dialog --backtitle "Foclabroc Toolbox" --title "Pack Kodi" \
 Attention : l'installation du pack supprimera tout le dossier .kodi 
 y compris tous vos paramètres actuels de Kodi, et les remplacera par ceux de mon pack.
 
-Êtes-vous sûr de vouloir installer le pack ?" 15 60
+\nÊtes-vous sûr de vouloir installer le pack ?" 15 60
 
 # Vérifier la réponse de l'utilisateur
 if [ $? -ne 0 ]; then
@@ -23,23 +24,24 @@ fi
 
 # Vérifier si le dossier Kodi existe, puis le supprimer
 if [ -d "$KODI_DIR" ]; then
-    echo "Suppression du dossier Kodi..."
+    echo "Suppression du dossier .Kodi..."
     rm -rf "$KODI_DIR"
     echo "Dossier supprimé."
 fi
 
 # Télécharger le fichier ZIP
 echo "Téléchargement du fichier ZIP..."
-wget -O /tmp/kodi.zip "$ZIP_URL"
+wget -q --show-progress -O /tmp/kodi.zip "$ZIP_URL"
 
 # Vérifier si le téléchargement a réussi
 if [ $? -eq 0 ]; then
     echo "Téléchargement réussi. Extraction en cours..."
-    unzip /tmp/kodi.zip -d /userdata/system/
+    unzip -o /tmp/kodi.zip -d /userdata/system/ | pv -lep -s $(unzip -l /tmp/kodi.zip | wc -l) > /dev/null
     echo "Extraction terminée."
     sleep 2
 else
     echo "Échec du téléchargement."
+    rm -f /tmp/kodi.zip
     exit 1
 fi
 
