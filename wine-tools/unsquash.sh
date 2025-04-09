9c016b9b919fa0e23ec9f2be6b1c913150177c31
foclabroc <foclabroc@hotmail.fr>
2025-04-09 09:17:46 +0200

    ...

--- wine-tools/unsquash.sh
+++ wine-tools/unsquash.sh
@@ -37,10 +37,10 @@ case "$extension" in
     final_dir="/userdata/roms/windows/${base_name}.wine"
     rm -rf "$final_dir"
     mkdir -p "$final_dir"
-	tar -xzf "$selected_file" -C "$final_dir" >/dev/tty 2>&1
+    tar -xzf "$selected_file" -C "$final_dir" >/dev/tty 2>&1
     if [ $? -ne 0 ]; then
       dialog --backtitle "Foclabroc Toolbox" --infobox "\nErreur de décompression du .TGZ répertoire deja existant..." 6 60 2>&1 >/dev/tty
-	  sleep 2
+      sleep 2
       curl -Ls https://raw.githubusercontent.com/foclabroc/toolbox/refs/heads/main/wine-tools/unsquash.sh | bash
     fi
     dialog --backtitle "Foclabroc Toolbox" --msgbox "Decompression effectué avec succès !\n\nEmplacement: $final_dir" 8 60 2>&1 >/dev/tty
@@ -49,6 +49,7 @@ case "$extension" in
     dialog --backtitle "Foclabroc Toolbox" --infobox "\nDécompression du fichier SquashFS (wsquashfs)... Veuillez patienter." 6 50 2>&1 >/dev/tty
     base_name=$(basename "$selected_file" .wsquashfs)
     final_dir="/userdata/roms/windows/${base_name}.wine"
+    rm -rf "$final_dir"
     unsquashfs -d "$final_dir" "$selected_file" 2>&1 >/dev/tty
     if [ $? -ne 0 ]; then
       dialog --backtitle "Foclabroc Toolbox" --infobox "\nErreur de décompression du .SquashFS répertoire deja existant..." 6 70 2>&1 >/dev/tty
