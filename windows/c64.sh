1ef547e9d8a1e2246d6a8cc96a599ed043fbbe1c
foclabroc <foclabroc@hotmail.fr>
2025-04-14 07:01:17 +0200

    ...

--- windows/c64.sh
+++ windows/c64.sh
@@ -14,7 +14,7 @@ afficher_barre_progression() {
         echo "20"; sleep 0.5
         curl -L --progress-bar "$URL_TELECHARGEMENT" -o "$FICHIER_ZIP"
         echo "60"; sleep 0.5
-        unzip -o "$FICHIER_ZIP" -d "$DOSSIER_DESTINATION"
+        unzip -o "$FICHIER_ZIP" -d "$DOSSIER_DESTINATION" > /dev/null 2>&1
         echo "80"; sleep 0.5
         rm "$FICHIER_ZIP"
         echo "90"; sleep 0.5
@@ -26,13 +26,13 @@ EOF
         chmod +x "$CHEMIN_SCRIPT"
         echo "100"; sleep 0.5
     ) |
-    dialog --title "Installation de Celeste64" --gauge "Téléchargement et installation en cours..." 10 60 0 2>&1 >/dev/tty
+    dialog --title "Installation de Celeste64" --gauge "\nTéléchargement et installation en cours..." 10 60 0 2>&1 >/dev/tty
 }
 
 # Exécution
 afficher_barre_progression
 
 # Message de fin
-dialog --title "Installation terminée" --msgbox "Celeste64 a été ajouté dans les Ports !\n\nPensez à mettre à jour vos gamelists pour le voir apparaître dans le menu." 10 50 2>&1 >/dev/tty
+dialog --title "Installation terminée" --msgbox "\nCeleste64 a été ajouté dans les Ports !\n\nPensez à mettre à jour vos gamelists pour le voir apparaître dans le menu." 10 50 2>&1 >/dev/tty
 
 clear
