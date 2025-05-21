bf9b1860ca517ae48277818b918ee70c9a2a0c31
foclabroc <foclabroc@hotmail.fr>
2025-05-21 09:40:16 +0200

    ...

--- updatebat/updatebat.sh
+++ updatebat/updatebat.sh
@@ -222,7 +222,7 @@ VERSION=$(batocera-es-swissknife --version | awk '{print $1}' | sed -E 's/^([0-9
 
 # Confirmation après sélection de version
 confirmer_version() {
-  dialog --backtitle "$BACKTITLE" --title "Confirmation" --yesno "\nVersion de Batocera actuelle : $VERSION\n\
+  dialog --backtitle "$BACKTITLE" --title "Confirmation" --yesno "\nVersion de Batocera actuelle : $VERSION\n\n\
 Voulez-vous installer la version $numero_version ?" 9 60 2>&1 >/dev/tty
 
   if [ $? -ne 0 ]; then
