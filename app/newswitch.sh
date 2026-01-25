1d461f673d81cb9d725f7f501d8532033ccf141e
foclabroc <foclabroc@hotmail.fr>
2026-01-25 09:44:22 +0100

    link dlc/update folder

--- app/newswitch.sh
+++ app/newswitch.sh
@@ -395,9 +395,9 @@ backup_switch_data() {
 	[ "$(ls -A /userdata/system/configs/yuzu/load 2>/dev/null)" ] && \
 	mv /userdata/system/configs/yuzu/load/* /userdata/tmp/tmp_yuzu_mods/ 2>/dev/null
 
-	[ -d "/userdata/system/configs/yuzu/nand/user/save" ] && \
-	[ "$(ls -A /userdata/system/configs/yuzu/nand/user/save 2>/dev/null)" ] && \
-	mv /userdata/system/configs/yuzu/nand/user/save/* /userdata/tmp/tmp_yuzu_save_user/ 2>/dev/null
+	[ -d "/userdata/system/configs/yuzu/nand/user" ] && \
+	[ "$(ls -A /userdata/system/configs/yuzu/nand/user 2>/dev/null)" ] && \
+	mv /userdata/system/configs/yuzu/nand/user/* /userdata/tmp/tmp_yuzu_save_user/ 2>/dev/null
 
 	[ -d "/userdata/system/configs/yuzu/nand/system/save" ] && \
 	[ "$(ls -A /userdata/system/configs/yuzu/nand/system/save 2>/dev/null)" ] && \
